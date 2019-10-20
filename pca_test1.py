ee7e074fa1b3aff0572e744565bc3764119c6985
ry3s <ktchg752@yahoo.co.jp>
2019-10-20 21:00:47 +0900

    finish all

---
+++ pca_test1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import pca
+
+
+Xy = []
+with open("winequality-red.csv") as fp:
+    for row in csv.reader(fp, delimiter=';'):
+        Xy.append(row)
+
+Xy = np.array(Xy[1:], dtype=np.float64)
+X = Xy[:, :-1]
+
+model = pca.PCA(n_components=2)
+model.fit(X)
+
+Y = model.transform(X)
+
+plt.scatter(Y[:, 0], Y[:, 1], color="k")
+plt.show()
